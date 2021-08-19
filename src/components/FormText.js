03dcd8a3ab95872bda7797e33452b684202b86e2
turab45 <turabbajeer202@gmail.com>
2021-08-19 21:07:29 +0500

    add reverse text logic

--- src/components/FormText.js
+++ src/components/FormText.js
@@ -10,18 +10,43 @@ export default function FormText() {
         console.log(text);
     }
 
-    const onClickHandler = () => {
+    const toUppercaseHandler = () => {
         let newText = text.toUpperCase();
         setText(newText);
+    }
+
+    const toLowercaseHandler = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const clearTextHandler = () => {
+        let newText = "";
+        setText(newText);
+    }
+
+    const inverseTextHandler = () => {
+        let arr = text.split("");
+        let reverseString = arr.reverse().join("");
+        
+        setText(reverseString);
     }
 
     return (
         <div>
             <div className="form-floating">
-                <h1 className="text-center p-5">Enter Text to analyze</h1>
+                <h1 className="text-center p-2">Enter Text to analyze</h1>
                 <textarea onChange={onChangeHandler} className="form-control" value={text} id="exampleFormControlTextarea1" rows="12" cols="10"></textarea>
             </div>
-            <button onClick={onClickHandler} type="button" className="btn btn-info mt-5">Convert To Uppercase</button>
+            <button onClick={toUppercaseHandler} type="button" className="btn btn-info mx-1 mt-5">Convert To Uppercase</button>
+            <button onClick={toLowercaseHandler} type="button" className="btn btn-info mx-1 mt-5">Convert To Lowercase</button>
+            <button onClick={clearTextHandler} type="button" className="btn btn-info mx-1 mt-5">Clear Text</button>
+            <button onClick={inverseTextHandler} type="button" className="btn btn-info mx-1 mt-5">Inverse Text</button>
+            <div className="my-3">
+                <h1>Your text summary</h1>
+                <p>{text.split(" ").length - 1} words and {text.length} characters.</p>
+                <p>{0.008 * text.split(" ").length} minutes read</p>
+            </div>
         </div>
     )
 }
